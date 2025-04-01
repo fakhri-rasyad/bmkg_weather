36f450a21b27e24f8eb0ee3fb41be6b8685e5d2f
fakhri-rasyad <kaizerd23@gmail.com>
2025-04-01 21:01:56 +0800

    feat: App Colors and Themes creation

---
+++ lib/presentation/core/themes/colors.dart
@@ -0,0 +1,7 @@
+abstract final class AppColors{
+  static const primary = "#ff0033cc";
+  static const primaryOffset = "#ff3388ff";
+  static const secondary = "#ff019900";
+  static const secondaryOffset = "#ff22cc00";
+  static const contrast = "#ffffffff";
+}
---
+++ lib/presentation/core/themes/theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+abstract final class AppTheme{
+  static const _textTheme = TextTheme(
+    headlineLarge: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+    headlineMedium: TextStyle(fontWeight: FontWeight.w300, fontSize: 16),
+    displayLarge: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+    labelMedium: TextStyle(fontWeight: FontWeight.normal, fontSize: 12)
+  );
+}
