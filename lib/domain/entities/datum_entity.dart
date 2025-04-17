24482de031fdd0e1c48a31f919dd7ea43ebff38d
fakhri-rasyad <kaizerd23@gmail.com>
2025-04-17 14:58:31 +0800

    feat: datum entity creation

--- lib/domain/entities/datum_entity.dart
+++ lib/domain/entities/datum_entity.dart
@@ -1,1 +1,17 @@
+import 'package:equatable/equatable.dart';
 
+import '../../data/model/cuaca.dart';
+import '../../data/model/lokasi.dart';
+
+class DatumEntity extends Equatable {
+  final Lokasi lokasi;
+  final List<List<Cuaca>> cuaca;
+
+  const DatumEntity({
+    required this.lokasi,
+    required this.cuaca,
+  });
+
+  @override
+  List<Object?> get props => [lokasi, cuaca];
+}
